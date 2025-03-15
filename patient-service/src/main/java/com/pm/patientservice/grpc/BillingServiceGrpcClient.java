c485898da144fb95effb5d31efb6e6285d7e7ba2
Shashank Hosahalli Shivamurthy <hsshashank.grad@gmail.com>
2025-03-15 16:14:01 -0700

    create BillingServiceGrpcCLient

---
+++ patient-service/src/main/java/com/pm/patientservice/grpc/BillingServiceGrpcClient.java
@@ -0,0 +1,47 @@
+package com.pm.patientservice.grpc;
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BillingServiceGrpcClient {
+
+    private static final Logger log = LoggerFactory.getLogger(BillingServiceGrpcClient.class);
+    private final BillingServiceGrpc.BillingServiceBlockingStub blockingStub;
+    //Synchronous client call - blockingstub waits for response from BillingService before continuing (Similar to
+    // REST but just grpc)
+
+
+    public BillingServiceGrpcClient(
+            @Value("${billing.service.address:localhost}") String serverAddress,
+            @Value("${billing.service.grpc.port:9001}") int serverPort) {
+
+        log.info("Connecting to Billing Service GRPC service at {}:{}", serverAddress, serverPort);
+
+        //This tells gRPC where to send requests.
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(serverAddress, serverPort)
+                .usePlaintext() // Uses unencrypted communication
+                .build();
+
+        // blockingStub is a gRPC client that sends requests to the Billing Service.
+        blockingStub = BillingServiceGrpc.newBlockingStub(channel);
+    }
+
+    public BillingResponse createBillingAccount(String patientId, String name,
+            String email) {
+
+        BillingRequest request = BillingRequest.newBuilder().setPatientId(patientId)
+                .setName(name).setEmail(email).build();
+
+        BillingResponse response = blockingStub.createBillingAccount(request);
+        log.info("Received response from billing service via GRPC: {}", response);
+        return response;
+    }
+}
